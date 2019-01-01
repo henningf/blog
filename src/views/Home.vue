0e05c5d041c6809043befa7bc5689287b243f250
Henning Fjellheim <henning.fjellheim@gmail.com>
2019-01-01 21:18:24 +0100

    started to make axios work

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,29 @@
 <template>
-  <v-container grid-list-md text-xs-center>
-    <v-slide-y-transition mode="out-in">
-      <v-layout row wrap>
-      <v-flex xs12 v-for="(post) in posts"
-            :key="post.index" >
-      <v-card >
-        <v-card-media
-          :src="post.featured_image"
-          height="200px"
-        ></v-card-media>
-
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">{{ post.title }}</h3>
-            <div>{{ post.summary }}</div>
-          </div>
-        </v-card-title>
-        <v-card-actions>
-          <v-btn router-link :to="'/post/' + post.slug" flat color="orange">View</v-btn>
-        </v-card-actions>
-      </v-card>
-
-      </v-flex>
-      </v-layout>
-    </v-slide-y-transition>
-  </v-container>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-container v-for="(post) in posts" :key="post.index">
+        <v-card v-on:click="goto_post">
+          <v-img class="white--text" height="200px" :src="img_src">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline">{{ post.Title }}</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+          <v-card-title>
+            <div>
+              <span class="grey--text">Date posted: {{ post.Created_at }}</span>
+              <br>
+              <span>{{ post_content }}</span>
+              <br>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-container>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -33,64 +32,46 @@ export default {
   data () {
     return {
       posts: [],
-      categories: []
+      categories: [],
+      img_src: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg',
+      post_content: 'This is my first blogpost, on this blog I\'ll write about ...... xx.xx.xxx'
     }
   },
+  /*
+  mounted () {
+    axios
+      .get('http://127.0.0.1:5000/api/v1.0/posts')
+      .then(response => (this.posts = response))
+  }, */
   methods: {
     getPosts () {
       // rewrite this, at first just return all posts this function needs to be written in the backend.
-      axios.get('http://127.0.0.1:5000/posts')
-      .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  })
-    }
-    /*
-    getPosts () {
-      butter.post.list({
-        page: 1,
-        page_size: 10
-      }).then((res) => {
-        // console.log(res.data)
-        this.posts = res.data.data
-      })
-      */
-    },
-    getCategories () {
-      butter.category.list()
-        .then((res) => {
-          console.log('List of Categories:')
-          console.log(res.data.data)
+      axios
+        .get('http://127.0.0.1:5000/api/v1.0/posts')
+        .then(response => (this.posts = response))
+        .catch(function (error) {
+          // handle error
+          console.log(error)
+        })
+        .then(function () {
+          // always executed
         })
     },
-    getPostsByCategory () {
-      butter.category.retrieve('example-category', {
-        include: 'recent_posts'
-      })
-        .then((res) => {
-          console.log('Posts with specific category:')
-          console.log(res)
-        })
+    goto_post: function (event) {
+      // Rewrite this to do something more useful!
+      alert(this.post_url)
     }
   },
   created () {
     this.getPosts()
-    this.getCategories()
-    this.getPostsByCategory()
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
